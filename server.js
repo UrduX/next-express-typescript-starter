a930fa2e681ad3fbc682cb00044ce7eda2e366db
Eren BALCI <erenbalci3939@gmail.com>
2020-11-21 00:57:31 +0300

    Parameter name has changed.

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import next from "next";
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
-const nextJsHandler = nextApp.getRequestHandler();
+const nextHandler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(() => {
   const app = express();
 
-  app.all("*", (req, res) => nextJsHandler(req, res));
+  app.all("*", (req, res) => nextHandler(req, res));
 
   app.listen(port, () => console.log(`> Ready on http://localhost:${port}`));
 });
