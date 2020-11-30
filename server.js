9629aa0df7f49b90e4d26ffe6dec2afa90630887
Eren BALCI <erenbalci3939@gmail.com>
2020-11-30 14:04:22 +0300

    Start and build command were updated. DOTENV added to server.js file.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import next from "next";
+require("dotenv").config();
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
